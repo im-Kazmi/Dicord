4038cdfd89a3e1b7e9eb1c20d51095e339f8739f
im-Kazmi <abidkazmi729@gmail.com>
2023-11-23 11:43:19 +0500

    added prisma and uploadthing

---
+++ constants/index.ts
@@ -0,0 +1,127 @@
+export const fakeServers = [
+  {
+    id: "1",
+    name: "React Devs",
+    image: "/react.jpg",
+  },
+  {
+    id: "2",
+    name: "Danger People",
+    image: "/danger.jpg",
+  },
+  {
+    id: "3",
+    name: "Javascript Devs",
+    image: "/js.jpg",
+  },
+];
+
+export const fakeChannels = [
+  {
+    id: "1",
+    name: "general",
+    type: "text",
+    image: "https://placekitten.com/200/200",
+    users: [
+      {
+        id: "101",
+        username: "User1",
+        avatar: "/avatars/a.jpg",
+      },
+      {
+        id: "102",
+        username: "User2",
+        avatar: "/avatars/b.jpg",
+      },
+    ],
+  },
+  {
+    id: "2",
+    name: "design team",
+    type: "text",
+    image: "https://placekitten.com/201/201",
+    users: [
+      {
+        id: "201",
+        username: "Abid",
+        avatar: "/avatars/c.jpg",
+      },
+      {
+        id: "202",
+        username: "Kazmi",
+        avatar: "/avatars/d.jpg",
+      },
+      {
+        id: "203",
+        username: "Izhar",
+        avatar: "/avatars/f.jpg",
+      },
+      {
+        id: "204",
+        username: "Kivy",
+        avatar: "/avatars/g.jpg",
+      },
+      {
+        id: "205",
+        username: "Smith",
+        avatar: "/avatars/h.png",
+      },
+    ],
+  },
+  {
+    id: "3",
+    name: "gaming voice",
+    type: "voice",
+    image: "https://placekitten.com/202/202",
+    users: [
+      {
+        id: "301",
+        username: "User5",
+        avatar: "/avatars/i.jpg",
+      },
+      {
+        id: "302",
+        username: "User6",
+        avatar: "/avatars/k.jpg",
+      },
+    ],
+  },
+];
+
+export const fakeUsers = [
+  {
+    id: "201",
+    username: "Abid",
+    avatar: "/avatars/c.jpg",
+  },
+  {
+    id: "202",
+    username: "Kazmi",
+    avatar: "/avatars/d.jpg",
+  },
+  {
+    id: "203",
+    username: "Izhar",
+    avatar: "/avatars/f.jpg",
+  },
+  {
+    id: "204",
+    username: "Kivy",
+    avatar: "/avatars/g.jpg",
+  },
+  {
+    id: "205",
+    username: "Smith",
+    avatar: "/avatars/h.png",
+  },
+  {
+    id: "301",
+    username: "User5",
+    avatar: "/avatars/i.jpg",
+  },
+  {
+    id: "302",
+    username: "User6",
+    avatar: "/avatars/k.jpg",
+  },
+];
---
+++ lib/actions/server.action.ts
@@ -0,0 +1,53 @@
+"use server";
+import prisma from "@/prisma";
+import { auth } from "@clerk/nextjs";
+import { revalidatePath } from "next/cache";
+import { v4 as uuid } from "uuid";
+
+interface CreateServer {
+  name: string;
+  imageUrl: string;
+}
+export async function createServer(params: CreateServer) {
+  try {
+    const { name, imageUrl } = params;
+    const { userId } = auth();
+
+    if (!userId) {
+      throw new Error("UnAuthorized");
+    }
+    await prisma.server.create({
+      data: {
+        name,
+        imageUrl,
+        profileId: userId as string,
+        inviteCode: uuid(),
+        channels: {
+          create: [{ name: "general", profileId: userId as string }],
+        },
+        members: {
+          create: [{ profileId: userId, role: "ADMIN" }],
+        },
+      },
+      include: {
+        members: true,
+        channels: true,
+      },
+    });
+
+    revalidatePath("/app");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getServers() {
+  try {
+    const servers = await prisma.server.findMany();
+
+    console.log(servers);
+    return servers;
+  } catch (error) {
+    console.log(error);
+  }
+}
